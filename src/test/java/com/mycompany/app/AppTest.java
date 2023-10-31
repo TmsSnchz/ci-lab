8420ba5102f5ea65f9ec5db97a3fc39614f25362
Replit user <>
2023-10-31 01:09:45 +0000

    cambio 4

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class AppTest {
   @Test
   public void probarTriangulo() {
-    assertEquals("error no es un triangulo", App.triangulo(2, 2, 3));
+    assertEquals("error no es un triangulo", App.triangulo(, 2, 3));
     assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
     // assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
     // assertEquals("es un triangulo escaleno", App.triangulo(3, 4, 6));
